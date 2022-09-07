05ed3ec56f0ec8acf53bc97b0ee65a969c401c97
rizwanc018 <22193871+rizwanc018@users.noreply.github.com>
2022-09-07 15:23:24 +0530

    build

--- src/assets/components/Headline.jsx
+++ src/assets/components/Headline.jsx
@@ -1,7 +1,7 @@
 export default function Headline() {
     return (
         <div className="headline">
-            <h2>Laura S Smith</h2>
+            <h2>Laura Smith</h2>
             <h4>Frontend Developer</h4>
             <small>laurasmith.website</small>
         </div>
